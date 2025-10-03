66356d3a125f5f1c66c3b99b9320daaff5766538
Hoang257 <quochoangtran36@gmail.com>
2025-10-03 15:40:35 +0300

    Update 02_sum_avg.py

--- src/02_sum_avg.py
+++ src/02_sum_avg.py
@@ -1,5 +1,4 @@
 first_value = float(input('Введите превое число: '))
 second_value = float(input('Введите второе число: '))
-sum = first_value + second_value
 avg = round((first_value + second_value)/2,2)
 print(sum, avg)
