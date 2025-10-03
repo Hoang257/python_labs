3438dd048bfe5ec466f126c41a457f8126edacbb
Hoang257 <quochoangtran36@gmail.com>
2025-10-03 15:40:39 +0300

    Update 05_initials_and_len.py

--- src/05_initials_and_len.py
+++ src/05_initials_and_len.py
@@ -2,7 +2,4 @@ name = str(input('ФИО: ')).strip().title()
 name = ' '.join(name.split())
 initials = name.split()
 initials = ''.join(initials[0] for initials in initials)
-
-
-
 print(len(f'{name}'), initials)
