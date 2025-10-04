b82a2873284f7679db4b28633f1e20edf6cc5b32
Hoang257 <quochoangtran36@gmail.com>
2025-10-04 11:25:47 +0300

    Update 02_sum_avg.py

--- src/02_sum_avg.py
+++ src/02_sum_avg.py
@@ -1,4 +1,10 @@
-first_value = float(input('Введите превое число: '))
-second_value = float(input('Введите второе число: '))
-avg = round((first_value + second_value)/2,2)
+first_value = str(input('Введите превое число: '))
+second_value = str(input('Введите второе число: '))
+if ',' in first_value or second_value:
+    first_value = first_value.replace(',', '.')
+    second_value = second_value.replace(',', '.')
+a = float(first_value)
+b = float(second_value)
+avg = round((a + b)/2,2)
+sum = a + b
 print(sum, avg)
