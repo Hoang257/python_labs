d0fe116586b2543a374b697502a2ed68c5bf90fe
Hoang257 <quochoangtran36@gmail.com>
2025-10-16 20:23:59 +0300

    Create text_stats.py

---
+++ src/lab03/src/lib/src/text_stats.py
@@ -0,0 +1,47 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from src.text import normalize, tokenize, count_freq, top_n
+
+TABLE_MODE = True
+def print_table(top_items):
+    
+    max_len_word = max(len(word) for word, _ in top_items)
+    col_1 = "слово"
+    col_2 = "частота"
+    width = max(max_len_word, len(col_1))
+
+    print('слово' + ' '* ((width+4)-len(col_1)) + "| частота" )
+
+
+    print("-"*(width+4)*2)
+    for word, count in top_items:
+
+        print(f"{word}" + ' ' * ((width+4)-len(word)) + f'| {count}')
+
+def main():
+    text = sys.stdin.readline().strip()
+    normalized = normalize(text)
+    tokens = tokenize(normalized)
+    freq = count_freq(tokens)
+
+    total_words = len(tokens)
+    unique_words = len(set(tokens))
+    top_5 = top_n(freq,5)
+    all_words = count_freq(tokens)
+
+    # print(f"Всего слов: {total_words}")
+    # print(f"Уникальных слов: {unique_words}")
+    # print("Топ-5:")
+    # for word, count in top_5:
+    #     print(f"{word}:{count}")
+    
+    if TABLE_MODE:
+        print_table(top_5)
+    else:
+        for word, count in top_5:
+            print(f"{word}: {count}")
+
+
+if __name__ == "__main__":
+    print(f"Табличный режим: {'ВКЛ' if TABLE_MODE else 'ВЫКЛ'}")
+    main()
