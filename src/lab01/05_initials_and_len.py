09946523b048a567679a52fdccf1b8e3452aad01
Hoang257 <quochoangtran36@gmail.com>
2025-10-16 20:23:45 +0300

    Create 05_initials_and_len.py

---
+++ src/lab01/05_initials_and_len.py
@@ -0,0 +1,5 @@
+name = str(input('ФИО: ')).strip().title()
+name = ' '.join(name.split())
+initials = name.split()
+initials = ''.join(initials[0] for initials in initials)
+print(len(f'{name}'), initials)
