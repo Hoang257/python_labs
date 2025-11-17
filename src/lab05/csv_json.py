f11809fcf14a28762be43bd6453ebed8ac53548b
Hoang257 <quochoangtran36@gmail.com>
2025-11-17 20:19:06 +0300

    Create csv_json.py

---
+++ src/lab05/csv_json.py
@@ -0,0 +1,29 @@
+import json 
+import csv
+import sys
+from pathlib import Path
+def csv_to_json(csv_path: str, json_path: str) -> None:
+    current_file = Path(__file__)
+    project_root = current_file.parent.parent.parent.parent
+
+    input_file = project_root / csv_path
+    output_file = project_root / json_path
+
+    if not input_file.exists():
+        raise FileNotFoundError('Файл не существует')
+    if not csv_path.lower().endswith('.csv'):
+        raise ValueError('Некоректный формат файла')
+    
+    data = []
+    try:
+        with open(input_file, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                data.append(row)
+    except UnicodeDecodeError:
+        raise UnicodeDecodeError('Некорректная кодировка файла')
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+    with open(output_file, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=2)
+
+csv_to_json(r'python_labs\data_lab_05\people.csv', r'python_labs\data_lab_05\people_from_csv.json')
