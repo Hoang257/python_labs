0d2cd52a48c31a1383e3060e6196d6cfce6fa56c
Hoang257 <quochoangtran36@gmail.com>
2025-11-28 19:49:44 +0300

    Update 04_minutes_to_hhmm.py

--- src/lab01/04_minutes_to_hhmm.py
+++ src/lab01/04_minutes_to_hhmm.py
@@ -1,6 +1,6 @@
-minutes = int(input('Минуты: '))
-hour = minutes//60
+minutes = int(input("Минуты: "))
+hour = minutes // 60
 min = minutes % 60
-if min < 10: 
-    min = '0' + f'{min}'
-print(f'{hour}:{min}')
+if min < 10:
+    min = "0" + f"{min}"
+print(f"{hour}:{min}")
