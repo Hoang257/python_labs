4d8fd908648f2b1ca4dcaac9556bfaf98771c06e
Hoang257 <quochoangtran36@gmail.com>
2025-11-28 19:58:30 +0300

    Done

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -7,22 +7,22 @@ def min_max(nums: List[Union[float, int]]) -> Tuple[Union[float, int]]:
     return min(nums), max(nums)
 
 
-print("\nТест min_max: ")
-print(min_max([3, -1, 5, 5, 0]))
-print(min_max([42]))
-print(min_max([]))
-print(min_max([1.5, 2, 2.0, -3.1]))
+# print("\nТест min_max: ")
+# print(min_max([3, -1, 5, 5, 0]))
+# print(min_max([42]))
+# print(min_max([]))
+# print(min_max([1.5, 2, 2.0, -3.1]))
 
 
 def unique_sorted(nums: List[float | int]) -> List[float | int]:
     return sorted(set(nums))  # возвращаем отсортированный список
 
 
-print("\nТест unique_sorted:")
-print(unique_sorted([3, 1, 2, 1, 3]))
-print(unique_sorted([]))
-print(unique_sorted([-1, -1, 0, 2, 2]))
-print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+# print("\nТест unique_sorted:")
+# print(unique_sorted([3, 1, 2, 1, 3]))
+# print(unique_sorted([]))
+# print(unique_sorted([-1, -1, 0, 2, 2]))
+# print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
 
 
 def flatten(mat: List[List | Tuple]) -> List:
@@ -34,8 +34,8 @@ def flatten(mat: List[List | Tuple]) -> List:
     return result
 
 
-print("\nТест flatten:")
-print(flatten([[1, 2], [3, 4]]))
-print(flatten([[1, 2], (3, 4, 5)]))
-print(flatten([[1, 2], (3, 4, 5)]))
-print(flatten([[1, 2], "ab"]))
+# print("\nТест flatten:")
+# print(flatten([[1, 2], [3, 4]]))
+# print(flatten([[1, 2], (3, 4, 5)]))
+# print(flatten([[1, 2], (3, 4, 5)]))
+# print(flatten([[1, 2], "ab"]))
