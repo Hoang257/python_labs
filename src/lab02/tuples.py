bed5689969820b002b721a58ee7ce943fef327ad
Hoang257 <quochoangtran36@gmail.com>
2025-11-28 19:49:58 +0300

    Update tuples.py

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -10,16 +10,17 @@ def format_record(rec: tuple([str, str, float])) -> str:
     if not isinstance(rec[2], float):
         return TypeError("3 элемент должен быть плавающим числом")
     name, group, gpa = rec
-    name_set = ' '.join(name.strip().split()).title()
+    name_set = " ".join(name.strip().split()).title()
     parts_name = name_set.split()
     if len(parts_name) < 2:
-        raise ValueError('ФИО дожен быть длинее двух слов')
+        raise ValueError("ФИО дожен быть длинее двух слов")
     surname = parts_name[0]
-    initials = [x[0] + '.' for x in parts_name[1:]]
+    initials = [x[0] + "." for x in parts_name[1:]]
     name_end = f"{surname} {''.join(initials)}"
-    group_set= group
-    gpa_set = f'{gpa:.2f}'
-    return f'{name_end}, гр. {group_set}, GPA {gpa_set}'
+    group_set = group
+    gpa_set = f"{gpa:.2f}"
+    return f"{name_end}, гр. {group_set}, GPA {gpa_set}"
+
 
 # result = format_record(student_data)
 # print(result)
@@ -32,6 +33,6 @@ if __name__ == "__main__":
         ("Петров Пётр Петрович", "IKBO-12"),
         ("  сидорова  анна   сергеевна ", "ABB-01", 3.999),
     ]
-    
+
     for test in test_cases:
         print(format_record(test))
